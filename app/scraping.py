cc66732ff6198392c6d1541e8adcb96d792cba84
takumiiimo <assistant2370@gmail.com>
2021-06-24 17:37:07 +0900

    SQLAlchemy DBを取り込む

--- app/scraping.py
+++ app/scraping.py
@@ -1,6 +1,10 @@
+import re
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+
+from assets.database import db_session
+from assets.models import Data
+
 import datetime
 
 def get_udemy_info():
@@ -29,20 +33,19 @@ def get_udemy_info():
     return  results
 
 def write_data():
-    df = pd.read_csv('assets/data.csv')
-
+    # 新規データの読み込み
     _results = get_udemy_info()
 
     # 今日の日付、受講者数、レビュー数を取得
-    date = datetime.datetime.today().strftime('%Y/%-m/%-d')
+    date = datetime.date.today()
     subscribers = _results['n_subscribers']
     reviews = _results['n_reviews']
 
-    results = pd.DataFrame([[date, subscribers, reviews]], columns=['date', 'subscribers', 'reviews'])
+    row = Data(date=date, subscribers=subscribers, reviews=reviews)
+    
+    db_session.add(row)
+    db_session.commit()
 
-    df = pd.concat([df, results])
-
-    df.to_csv('assets/data.csv', index=False)    
 
 if __name__ == "__main__":
     write_data()
--- app/view.py
+++ app/view.py
@@ -7,32 +7,26 @@ import plotly.graph_objs as go
 import pandas as pd
 import datetime
 
+from assets.database import db_session
+from assets.models import Data
 
 # 外部のスタイルシートを受け取る
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-df = pd.read_csv('assets/data.csv')
 
-# date型に変更
-import datetime
-
-datetime.datetime.strptime(df['date'][44],'%Y/%m/%d').date()
+data = db_session.query(Data.date, Data.subscribers, Data.reviews).all()
 
 dates = []
-for _date in df['date']:
-    date = datetime.datetime.strptime(_date,'%Y/%m/%d').date()
-    dates.append(date)
+subscribers = []
+reviews = []
 
-# 受講生の数
-n_subscribers = df['subscribers'].values
+for datum in data:
+    dates.append(datum.date)
+    subscribers.append(datum.subscribers)
+    reviews.append(datum.reviews)
 
-n_reviews = df['reviews'].values
-
-# 受講生の差分
-diff_subscribers = df['subscribers'].diff().values
-
-# レビューの差分
-diff_reviews = df['reviews'].diff().values
+diff_subscribers = pd.Series(subscribers).diff().values
+diff_reviews = pd.Series(reviews).diff().values
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -45,7 +39,7 @@ app.layout=html.Div(children=[
                 'data': [
                     go.Scatter(
                         x=dates,
-                        y=n_subscribers,
+                        y=subscribers,
                         mode='lines+markers',
                         name='受講生総数',
                         opacity=0.7,
@@ -62,7 +56,7 @@ app.layout=html.Div(children=[
                     title='受講生総数の推移',
                     xaxis=dict(title='date'),
                     yaxis=dict(title='受講生総数', side='left', showgrid=False,
-                        range=[2500, max(n_subscribers)+ 100]),
+                        range=[2500, max(subscribers)+ 100]),
                     yaxis2=dict(title='増加人数', side='right', overlaying='y', showgrid=False,
                         range=[0,max(diff_subscribers[1:])+ 100]), 
                     margin=dict(l=200, r=200,t=100)
@@ -75,7 +69,7 @@ app.layout=html.Div(children=[
                 'data': [
                     go.Scatter(
                         x=dates,
-                        y=n_reviews,
+                        y=reviews,
                         mode='lines+markers',
                         name='レビュー総数',
                         opacity=0.7,
@@ -92,7 +86,7 @@ app.layout=html.Div(children=[
                     title='レビュー総数の推移',
                     xaxis=dict(title='date'),
                     yaxis=dict(title='レビューの総数', side='left', showgrid=False,
-                        range=[200, max(n_reviews)+ 100]),
+                        range=[200, max(reviews)+ 100]),
                     yaxis2=dict(title='増加数', side='right', overlaying='y', showgrid=False,
                         range=[0,max(diff_reviews[1:])+ 10]), 
                     margin=dict(l=200, r=200,t=100)
